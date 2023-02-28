bca1d574855c4b1628fc48dfd4893516d9b83d79
Yoyo9506130613 <110320194+Yoyo9506130613@users.noreply.github.com>
2023-02-28 21:32:45 +0800

    cool
    cool

---
+++ cool.py
@@ -0,0 +1,13 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index2.html")
+@app.route("/cool")
+def cool():
+    return render_template("index3.html")              
+
+if __name__ == "__main__":
+    app.run(debug=True)
